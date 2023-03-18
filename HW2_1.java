fe5bb834ebe5f97410bc5137f130d52400fcd964
Zhanar <zhanarkz@gmail.com>
2023-03-18 22:37:39 +0600

    ДЗ 2го семинара

---
+++ HW2_1.java
@@ -0,0 +1,39 @@
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+// Дана строка sql-запроса "select * from students where ". 
+// Сформируйте часть WHERE этого запроса, используя StringBuilder. 
+// Данные для фильтрации приведены ниже в виде json строки.
+// Если значение null, то параметр не должен попадать в запрос.
+// Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
+
+public class HW2_1 {
+    public static void main(String[] args) {
+        Map<String, String> map = new LinkedHashMap<>();
+        map.put("name","Ivanov");
+        map.put("country","Russia");
+        map.put("city","Moskow");
+        map.put("age",null);
+
+        System.out.println(getQuery(map));
+    }
+
+    public static String getQuery(Map<String, String> params) {
+        StringBuilder result = new StringBuilder();
+        if (params == null || params.isEmpty())
+            return result.toString();
+
+        for (Map.Entry<String, String> pair : params.entrySet()) {
+            if (pair.getKey() == null || pair.getValue() == null)
+                continue;
+
+            result.append(pair.getKey()).append(" = '").append(pair.getValue()).append("' and ");
+        }
+
+        if (result.length() > 5)
+            result.delete(result.length() - 5, result.length());
+
+        return result.toString();
+    }
+}
